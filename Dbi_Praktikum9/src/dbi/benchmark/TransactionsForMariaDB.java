bced10939cefc4015f0fa54476bd15d0984b39e1
Stefan Guelland <stefan@sguelland.de>
2013-12-11 14:49:49 +0100

    sync
    Signed-off-by: Stefan Guelland <stefan@sguelland.de>

--- Dbi_Praktikum9/src/dbi/benchmark/TransactionsForMariaDB.java
+++ Dbi_Praktikum9/src/dbi/benchmark/TransactionsForMariaDB.java
@@ -41,9 +41,18 @@ public class TransactionsForMariaDB extends Database
 	}
 	
 	public void einzahlungTransaction()
-	{
-		
-	}
+    {
+        int kontostand;
+        try {
+            kontostand=einzahlungTX("3","1","2",500);
+              
+            System.out.println(kontostand);
+              
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    } 
 	
 	public void analyseTransaction()
 	{
@@ -67,12 +76,55 @@ public class TransactionsForMariaDB extends Database
 		
 	}
 	
-	public void einzahlungTX(String accid, String tellerid, String branchid, String delta) throws SQLException
-	{
-		Statement statement = connection.createStatement();
-		statement.executeQuery("SELECT balance FROM branches WHERE branchid=4;");
-		statement.close();
-	}
+	public int einzahlungTX(String accid, String tellerid, String branchid, int delta) throws SQLException
+    {
+        Statement statement = connection.createStatement();
+        /**
+         * balance in branches mit der speziellen branchid aktualisieren
+         */
+        ResultSet res = statement.executeQuery("SELECT balance FROM branches WHERE branchid='" + branchid + "'");
+          
+        /* Abfrage ob ein Eintrag verfügbar ist. */
+        if(res.next()){
+          
+            statement.executeQuery("UPDATE branches SET balance=" + (delta+res.getInt(1)) + " WHERE branchid='" + branchid + "';");
+        }
+        else
+            return 0; /* Rückgabe bei Fehler in Datenbank - ÄNDERN -> throw? return 0? */
+          
+        /**
+         * balance in tellers mit der speziellen tellerid aktualisieren
+         */
+        res = statement.executeQuery("SELECT balance FROM tellers WHERE tellerid='" + tellerid + "';");
+          
+        if(res.next()){
+            statement.executeQuery("UPDATE tellers SET balance=" + (delta+res.getInt(1)) + " WHERE tellerid='" + tellerid + "';");
+        }
+        else
+            return 0;
+          
+        /**
+         * balance in accounts mit der speziellen accid aktualisieren
+         */
+        res = statement.executeQuery("SELECT balance FROM accounts WHERE accid='" + accid + "';");
+          
+        if(res.next()){
+            statement.executeQuery("UPDATE accounts SET balance=" + (delta+res.getInt(1)) + " WHERE accid='" + accid + "';");
+        }
+        else
+            return 0;
+        statement.executeQuery("INSERT INTO history VALUES ('"+ accid +"','"+ tellerid +"',"+ delta +",'"+ branchid +"','"+ res.getInt(1) +"','"+ "x" +"')");//cmmnt char
+          
+        /**
+         * akutelles balance aus der datenbank ziehen
+         */
+        res = statement.executeQuery("SELECT balance FROM accounts WHERE accid='" + accid + "';");
+        res.next();
+          
+        statement.close();
+          
+        return res.getInt(1);
+    } 
 	
 	public void analyseTX(long delta) throws SQLException
 	{
